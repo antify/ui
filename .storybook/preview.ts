ccf227989cd167088b3e797620106692a8b9ddf7
markusbink <bink-markus@web.de>
2025-02-05 13:54:11 +0100

    refactor: Update tailwindcss
    Removes dependency to postcss in place of its own vite plugin.

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,4 @@
 import {type Preview, setup} from "@storybook/vue3";
-import 'tailwindcss/tailwind.css';
 import './assets/storybook.css';
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
 
