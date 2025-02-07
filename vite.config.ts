208409df3b75769eeb4a631663f8e611ac773592
markusbink <bink-markus@web.de>
2025-02-07 14:31:47 +0100

    chore: Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
-import {defineConfig} from 'vite'
-import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
+import {
+  defineConfig,
+} from 'vite';
+import vue from '@vitejs/plugin-vue';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +10,9 @@ export default defineConfig({
     vue(),
     tailwindcss(),
   ],
-})
+  optimizeDeps: {
+    exclude:  [
+      '@fortawesome/vue-fontawesome',
+    ],
+  },
+});
