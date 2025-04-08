78556889804e88de42823225cfcedc81240e5412
Johannes Beyer <johannes@blue-panda.de>
2025-04-08 10:50:35 +0200

    fix(AntColorInput): Fix type error

--- src/components/inputs/__types/index.ts
+++ src/components/inputs/__types/index.ts
@@ -5,4 +5,4 @@ export * from './AntSelect.types';
 export * from './AntSwitcher.types';
 export * from './AntTagInput.types';
 export * from './AntTextInput.types';
-export * from '@/components/inputs/AntColorInput/AntColorInput.types';
+export * from '../AntColorInput/AntColorInput.types';
