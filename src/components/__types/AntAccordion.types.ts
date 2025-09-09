1321fd619b727eac85dd46be4c0efeb69660e5ca
max <maximilian@blue-panda.de>
2025-09-09 08:48:26 +0200

    feat: add open items to prop (#208)

--- src/components/__types/AntAccordion.types.ts
+++ src/components/__types/AntAccordion.types.ts
@@ -2,3 +2,15 @@ export enum CollapseStrategy {
   single = 'single',
   multiple = 'multiple',
 }
+
+export interface AccordionItem {
+  label?: string;
+  content?: string;
+  isOpen?: boolean;
+  iconLeft?: boolean;
+  contentPadding?: boolean;
+  activeLabelClasses?: string;
+  activeIconClasses?: string;
+  inactiveLabelClasses?: string;
+  inactiveIconClasses?: string;
+}
